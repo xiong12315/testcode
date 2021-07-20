0b40bb60ea30cc0beb908be1032b45bedebe18c3
xiong12315 <xhp2014@163.com>
2021-07-20 17:41:34 +0800

    嘿嘿

--- src/components/rights/Rights.vue
+++ src/components/rights/Rights.vue
@@ -8,11 +8,43 @@
     </el-breadcrumb>
     <!-- 卡片视图 -->
     <el-card>
-      权限
+      <el-table :data="rightdata" style="width: 100%" border stripe>
+        <!-- 索引列 -->
+        <el-table-column type="index"> </el-table-column>
+        <el-table-column prop="authName" label="权限名称"> </el-table-column>
+        <el-table-column prop="path" label="路径"> </el-table-column>
+        <el-table-column prop="level" label="权限等级">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.level == 0">一级</el-tag>
+            <el-tag type="success" v-else-if="scope.row.level == 1">二级</el-tag>
+            <el-tag type="warning" v-else>三级</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      rightdata: []
+    };
+  },
+  created() {
+    this.getRightList();
+  },
+  methods: {
+    //获取权限列表
+    async getRightList() {
+      let { data: res } = await this.$http.get('rights/list');
+      // console.log(res);
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取权限失败');
+      }
+      this.rightdata = res.data;
+    }
+  }
+};
 </script>
 <style lang="scss" scoped></style>
--- src/components/rights/Roles.vue
+++ src/components/rights/Roles.vue
@@ -8,11 +8,63 @@
     </el-breadcrumb>
     <!-- 卡片视图 -->
     <el-card>
-      角色
+      <!-- 添加角色按钮区域 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+      <!-- 角色列表区域 -->
+      <el-table :data="RoleList" stripe border style="width: 100%">
+        <!-- 展开列区域 -->
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row v-for="item1 in scope.row.children" :key="item1.id">
+              <!-- 渲染是一级权限 -->
+              <el-col :span="5">
+                <el-tag>{{ item1.authName }}</el-tag>
+              </el-col>
+              <!-- 渲染是二级三级权限 -->
+              <el-col :span="19"></el-col>
+            </el-row>
+            <!-- pre是可以格式化树形结构 -->
+            <pre>{{ scope.row }}</pre>
+          </template>
+        </el-table-column>
+        <el-table-column type="index"> </el-table-column>
+        <el-table-column prop="roleName" label="角色名称"> </el-table-column>
+        <el-table-column prop="roleDesc" label="角色描述"> </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary" icon="el-icon-edit">编辑</el-button>
+            <el-button type="danger" icon="el-icon-delete">删除</el-button>
+            <el-button type="warning" icon="el-icon-setting">分配权限</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      RoleList: []
+    };
+  },
+  created() {
+    this.getRoleList();
+  },
+  methods: {
+    async getRoleList() {
+      let { data: res } = await this.$http.get('roles');
+      // console.log(res);
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色错误');
+      }
+      this.RoleList = res.data;
+    }
+  }
+};
 </script>
 <style lang="scss" scoped></style>
